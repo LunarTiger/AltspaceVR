ab7acb0cd85b4078154c96d63bfb85ba485dabf5
Ryan Fowler-Hughes <lunartiger@live.com>
2018-02-09 21:09:50 -0500

    Update and rename loadaframe.js to loadscripts.js

--- loadaframe.js
+++
@@ -1,3 +0,0 @@
-var the_aframe = document.createElement('script');
-the_aframe.setAttribute('src','https://aframe.io/releases/0.7.0/aframe.js');
-document.head.appendChild(the_aframe);
---
+++ loadscripts.js
@@ -0,0 +1,6 @@
+var the_aframe = document.createElement('script');
+the_aframe.setAttribute('src','https://aframe.io/releases/0.7.0/aframe.js');
+document.head.appendChild(the_aframe);
+var the_sdk = document.createElement('script');
+the_sdk.setAttribute('src','https://sdk.altvr.com/libs/altspace.js/2.7.4/altspace.min.js');
+document.head.appendChild(the_sdk);
